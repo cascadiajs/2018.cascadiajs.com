038f0fb859146758b28157446c0eec639b52fbea
Gavin Mogan <git@gavinmogan.com>
2018-06-08 22:16:29 -0700

    Show the entire mailchimp error message all html like

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,12 +54,36 @@ class MailChimpForm extends React.Component {
       <div>
         <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css"/>
         <div id="mc_embed_signup" style={{clear: 'left', width: '100%', fontFamily: 'sans-serif'}}>
-          <form onSubmit={this.handleSubmit} id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+          <form
+            onSubmit={this.handleSubmit}
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form" 
+            className="validate"
+            target="_blank"
+            noValidate
+          >
             <div id="mc_embed_signup_scroll">
-              <label htmlFor="mce-EMAIL">Subscribe to get updates on the upcoming CascadiaJS 2018 conference</label>
-              <input type="email" onChange={this.handleEmailChange} value={email} name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required/>
+              <label htmlFor="mce-EMAIL">
+                Subscribe to get updates on the upcoming CascadiaJS 2018 conference
+                {status === 'error' && <h2 style={{ color: '#cc0000' }} dangerouslySetInnerHTML={{ __html: msg }} />}
+              </label>
+              <input
+                type="email"
+                onChange={this.handleEmailChange}
+                value={email}
+                name="EMAIL"
+                className="email"
+                id="mce-EMAIL"
+                placeholder="email address"
+                required />
               <div className="clear">
-                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" style={{backgroundColor: '#376BFB'}}/>
+                <input
+                  type="submit"
+                  value="Subscribe"
+                  name="subscribe"
+                  id="mc-embedded-subscribe"
+                  className="button"
+                  style={{backgroundColor: '#376BFB'}} />
               </div>
             </div>
           </form>
