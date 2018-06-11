6d7bde3b1b6d52e61ae521177a657e870cf5f73b
Gavin Mogan <git@gavinmogan.com>
2018-06-10 19:56:23 -0700

    2015 apparently needs cascadiajs.org

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ const Footer = () => (
     <a title="CascadiaJS 2012" href="http://2012.cascadiajs.com">2012</a>
     <a title="CascadiaJS 2013" href="http://2013.cascadiajs.com">2013</a>
     <a title="CascadiaJS 2014" href="http://2014.cascadiajs.com">2014</a>
-    <a title="CascadiaJS 2015" href="http://2015.cascadiafest.org">2015</a>
+    <a title="CascadiaJS 2015" href="http://2015.cascadiajs.org">2015</a>
     <a title="CascadiaJS 2016" href="http://2016.cascadiafest.org">2016</a>
 
   </footer>
