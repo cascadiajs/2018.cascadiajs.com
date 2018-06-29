8eff7ca1d3a7273fe406dcb5b66411ac761cc681
Carter Rabasa <carter@rabasa.com>
2018-06-29 11:12:09 -0700

    fixed broken external link

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function IndexPage () {
         <div id="cta">
           <div><Link to="/call-for-presenters">Be a Speaker</Link></div>
           <div><Link to="/call-for-organizers">Be an Organizer</Link></div>
-          <div><Link to="https://ti.to/event-loop/cascadiajs-2018" title="Tickets On Sale Soon!">Buy Tickets</Link></div>
+          <div><a href="https://ti.to/event-loop/cascadiajs-2018" title="Tickets On Sale Soon!">Buy Tickets</a></div>
         </div>
         <MailChimpForm />
       </div>
