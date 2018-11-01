92fdd127f991ff73931ea81622bebd98fe2ba57c
Carter Rabasa <carter@rabasa.com>
2018-11-01 13:06:31 -0700

    added Edge

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,9 +131,9 @@ function IndexPage ({data}) {
             <div><a href="https://formidable.com/"><img src="/formidable.png" alt="Formidable logo"/></a></div>
             <div><a href="https://fizbuz.com/"><img src="/fizbuz.svg" alt="Fizbuz logo"/></a></div>
             <div><a href="https://twilio.com/"><img src="/logos/twilio.svg" alt="Twilio logo"/></a></div>
-            <div><a href="https://esri.com/"><img src="/logos/esri.svg" alt="Esri logo"/></a></div>
             <div><a href="https://developer.microsoft.com/en-us/advocates/index.html"><img src="/azure.png" alt="Azure logo"/></a></div>
             <div><a href="https://www.smartsheet.com"><img src="/logos/smartsheet.png" alt="Smartsheet logo"/></a></div>
+            <div><a href="https://blogs.windows.com/msedgedev/"><img src="/logos/ms-edge.png" alt="Microsoft Edge logo"/></a></div>
           </section>
           <section id="community">
             <div><a href="https://egghead.io/"><img src="/egghead-logo.png" alt="Egghead logo"/></a></div>
@@ -143,9 +143,7 @@ function IndexPage ({data}) {
             <div><a href="https://www.genui.co/"><img src="/logos/genui.jpg" alt="Gen/UI logo"/></a></div>
             <div><a href="https://www.slalom.com/"><img src="/slalom-logo.png" alt="Slalom logo"/></a></div>
             <div><a href="https://www.mistyrobotics.com/"><img src="/logos/misty.svg" alt="Misty Robotics logo"/></a></div>
-            <div>&nbsp;</div>
-            <div>&nbsp;</div>
-            <div>&nbsp;</div>
+            <div><a href="https://esri.com/"><img src="/logos/esri.svg" alt="Esri logo"/></a></div>
           </section>
         </section>
         <section id="contact">
